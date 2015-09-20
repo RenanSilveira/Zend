bc0b5cc09fde3b8c687c37681ee1fc7d26f8907f
Celso <Celso@Celso-PC>
2015-09-20 01:20:29 -0300

    Realizado a correção do redirecionamento nas páginas dos Usuário e na página Painel. Criado o recurso log e colocado para gerar log no console no IndexController

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,10 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    protected function _initLogger(){
+        $this->bootstrap('log');
+        Zend_Registry::setClassName('logger', $this->getResource('log'));
+    }
 
 }
 
