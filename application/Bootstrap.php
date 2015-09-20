8f160de31a4334b38a79cfabe3cf013f66883d15
Celso <Celso@Celso-PC>
2015-09-20 02:40:03 -0300

    Foi acertado o arquivo Bootstrapque continha um erro que não deixava nada ser exibido. Foi alterado a forma com que são incluidos os arquivos .js e .css colocando o caminho absoluto. Criado as actions New e Delete no Controller Usuario.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,8 +3,8 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
     protected function _initLogger(){
-        $this->bootstrap('log');
-        Zend_Registry::setClassName('logger', $this->getResource('log'));
+        $this->_bootstrap('log');
+        Zend_Registry::set('logger', $this->getResource('log'));
     }
 
 }
--- application/controllers/UsuarioController.php
+++ application/controllers/UsuarioController.php
@@ -3,16 +3,37 @@
 class UsuarioController extends Zend_Controller_Action
 {
 
+    /**
+     * @var Zend_Log
+     */
+    private $logger = null;
+
     public function init()
     {
         /* Initialize action controller here */
+        $this->logger = Zend_Registry::get('logger');
     }
 
     public function indexAction()
+    {
+        // action body
+        $this->logger->log('Mensagem debug', Zend_Log::DEBUG);
+    }
+
+    public function newAction()
+    {
+        // action body
+    }
+
+    public function deleteAction()
     {
         // action body
     }
 
 
 }
+
+
+
+
 
