a06dfcb2cc5ec21e841cfc59e73c18ddda789b74
Celso <Celso@Celso-PC>
2015-09-22 10:37:01 -0300

    Foi criado o Form para adicionar um usuário, mas ainda estamos verificando como o action funciona.

--- application/controllers/UsuarioController.php
+++ application/controllers/UsuarioController.php
@@ -1,41 +1,38 @@
 <?php
 
-class UsuarioController extends Zend_Controller_Action
-{
+class UsuarioController extends Zend_Controller_Action {
 
     /**
      * @var Zend_Log
      */
     private $logger = null;
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
         $this->logger = Zend_Registry::get('logger');
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
         $this->logger->log('Mensagem debug', Zend_Log::DEBUG);
         $dbUsuario = new Application_Model_DbTable_Usuario();
         $this->view->lista = $dbUsuario->fetchAll();
     }
 
-    public function newAction()
-    {
-        // action body
+    public function newAction() {
+        //cria um novo formulário
+        $this->view->form = new Application_Form_NewUsuario();
+
+        
     }
 
-    public function deleteAction()
-    {
+    public function deleteAction() {
         // action body
+        if ($this->getRequest()->isPost() and $form->isValid($_POST)) {
+            $values = $this->view->form->getValues();
+            $this->view->teste = $values->Nome;
+            //$this->_redirect( 'new' );
+        }
     }
-
 
 }
-
-
-
-
-
---
+++ application/forms/NewUsuario.php
@@ -0,0 +1,41 @@
+<?php
+
+class Application_Form_NewUsuario extends Zend_Form {
+
+    public function init() {
+        //nome do formulário
+        $this->setName('NewUsuario');
+        //elemento para o campo username
+        $nome = new Zend_Form_Element_Text('Nome');
+        $login = new Zend_Form_Element_Text('Login');
+        $pass = new Zend_Form_Element_Password('Password');
+
+        $nome->setLabel('Login')
+                ->setRequired(true)
+                ->addFilter('StripTags')
+                ->addValidator('NotEmpty');
+
+        $login->setLabel('Login')
+                ->setRequired(true)
+                ->addFilter('StripTags')
+                ->addValidator('NotEmpty');
+
+        $pass->setLabel('Senha')
+                ->setRequired(true)
+                ->addFilter('StripTags')
+                ->addValidator('NotEmpty');
+
+        //botão de submit
+        $submit = new Zend_Form_Element_Submit('submit');
+        $submit->setLabel('Entrar');
+        $submit->setAttrib('id', 'Entrar')->setIgnore(true);
+        $submit->setAttrib('class', 'btn btn-success');
+
+        //adicionar os campos ao formulário
+        $this->addElements(array($nome, $login, $pass, $submit));
+        
+        //action e method
+        $this->setAction('delete')->setMethod('post');
+    }
+
+}
