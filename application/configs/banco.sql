c9c366e6ec8503367a191043e5338a3c4afa151d
Celso <Celso@Celso-PC>
2015-09-22 09:59:55 -0300

    Foram alteradas algumas coisas no layout onde mostra os usuários cadastrados. Foram realizadas algumas alterações no banco de dados: Criação da tabela Tipo e adição da coluna tipo na tabela Usuario.

--- application/configs/banco.sql
+++ application/configs/banco.sql
@@ -5,5 +5,11 @@ create table usuario(
     nome text not null,
     login text not null,
     senha text not null,
-    ativo boolean not null
-    );
+    ativo boolean not null,
+    tipo int not null
+);
+
+create table tipo(
+    id serial not null primary key,
+    nome text not null
+);
